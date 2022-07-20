295208ac13c1b9ed88cd9a2d5f81832c39c3dea6
raphael94360 <paulberthet94@gmail.com>
2022-07-20 21:20:04 +0200

    modification parametre cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: "localhost:3000" }));
+app.use(cors({ origin: "localhost:4000" }));
 
 app.get("/", (req, res) => {
   return res.json({
